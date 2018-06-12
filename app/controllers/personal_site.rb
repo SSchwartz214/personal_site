00f4388c23f33320d7752fb9893b4fbd7de8732e
Seth Schwartz <sschwartz214@gmail.com>
2018-06-12 14:43:42 -0400

    adds launchy functionallity

---
+++ app/controllers/personal_site.rb
@@ -0,0 +1,8 @@
+require 'rack'
+
+class PersonalSite
+  def self.call(env)
+    # This array includes the HTTP response status code, HTTP response headers & HTTP body
+    ['200', {'Content-Type' => 'text/html'}, ['Welcome!']]
+  end
+end
